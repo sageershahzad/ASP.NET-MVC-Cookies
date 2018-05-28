8b998d7bc8d8be7f401e691cefeafe94b2b6f147
Sageer Shahzad <contact@shazplus.co.uk>
2018-05-28 03:11:44 +0100

    Cookies Added

---
+++ Cookies/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Cookies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            HttpCookie cookie = Request.Cookies["Name"];
+            if (cookie != null)
+            {
+                string name = Request.Cookies["Name"].Value;
+                ViewData["Name"] = name;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Visitor visitor)
+        {
+            if (ModelState.IsValid == true)
+            {
+                HttpCookie cookie = new HttpCookie("Name");
+                cookie.Value = visitor.Name;
+
+                HttpContext.Response.Cookies.Add(cookie);
+                cookie.Expires = DateTime.Now.AddDays(1);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Cookies/Models/Visitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cookies.Models
+{
+    public class Visitor
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+    }
+}
